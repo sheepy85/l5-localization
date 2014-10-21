da9b9ca977b089d308f4009d43523261e8dd34e4
sheepy85 <sheepy85@gmail.com>
2014-10-21 10:18:58 +0200

    add command for file generators

--- src/Sheepy85/L5Localization/L5LocalizationServiceProvider.php
+++ src/Sheepy85/L5Localization/L5LocalizationServiceProvider.php
@@ -1,56 +1,58 @@
-<?php namespace Sheepy85\L5Localization;
+<?php
+
+namespace Sheepy85\L5Localization;
 
 use Illuminate\Support\ServiceProvider;
 
 class L5LocalizationServiceProvider extends ServiceProvider {
 
-   /**
-	* Indicates if loading of the provider is deferred.
-	*
-	* @var bool
-	*/
-   protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-   /**
-	* Register the package's component namespaces.
-	*
-	* @return void
-	*/
-   public function boot() {
-	  $this->package( 'sheepy85/l5-localization' );
-   }
+    /**
+     * Register the package's component namespaces.
+     *
+     * @return void
+     */
+    public function boot() {
+        $this->package( 'sheepy85/l5-localization' );
+    }
 
-   /**
-	* Register the service provider, alias and command.
-	*
-	* @return void
-	*/
-   public function register() {
-	  $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-	  $loader->alias( 'Locale' , 'Sheepy85\L5Localization\Facades\Localization' );
+    /**
+     * Register the service provider, alias and command.
+     *
+     * @return void
+     */
+    public function register() {
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias( 'Locale' , 'Sheepy85\L5Localization\Facades\Localization' );
 
-	  $this->app[ 'l5-localization' ] = $this->app->share( function($app) {
-		 return new Localization( $app );
-	  } );
+        $this->app['l5-localization'] = $this->app->share( function($app) {
+            return new Localization( $app );
+        } );
 
-	  $this->app[ 'l5-router' ] = $this->app->share( function($app) {
-		 return new Router( $app );
-	  } );
+        $this->app['l5-router'] = $this->app->share( function($app) {
+            return new Router( $app );
+        } );
 
-	  $this->app[ 'command.l5-localization.select' ] = $this->app->share( function() {
-		 return new Console\Select;
-	  } );
+        $this->app['command.l5-localization.select'] = $this->app->share( function($app) {
+            return new Console\Select( $app['files'] );
+        } );
 
-	  $this->commands( 'command.l5-localization.select' );
-   }
+        $this->commands( 'command.l5-localization.select' );
+    }
 
-   /**
-	* Get the services provided by the provider.
-	*
-	* @return array
-	*/
-   public function provides() {
-	  return [ ];
-   }
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return [];
+    }
 
 }
